2435cc27ba436595ffeddfdf758331a55f807098
Arvind Thangamani <dnivra26@gmail.com>
2018-04-24 15:20:23 +0530

    fetch greeting from todo_hello_grpc and append with title

--- service/TodoService.go
+++ service/TodoService.go
@@ -5,6 +5,8 @@ import (
 	"todo_proto/pb/proto"
 	db2 "todo_grpc/db"
 	"strconv"
+	"google.golang.org/grpc"
+	"log"
 )
 
 type Server struct {
@@ -30,9 +32,21 @@ func (s *Server) CreateTodo(ctx context.Context, createTodoRequest *proto.Create
 
 func (s *Server) GetTodo(ctx context.Context, request *proto.GetTodoRequest) (*proto.GetTodoResponse, error) {
 	todo, err := s.dao.GetTodo(request.Id)
+	todo.Title, _ = getHello()
 	if (err != nil) {
 		return nil, err
 	} else {
 		return &proto.GetTodoResponse{Todo: &todo}, nil
 	}
+}
+
+func getHello() (string, error) {
+	conn, e := grpc.Dial(":7778", grpc.WithInsecure())
+	if e != nil {
+	log.Fatalf("did not connect: %s", e)
+	}
+	defer conn.Close()
+	client := proto.NewPingClient(conn)
+	response, err := client.SayHello(context.Background(), &proto.PingMessage{Greeting: "Hello"})
+	return response.Greeting, err
 }
